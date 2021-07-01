36faeffce9cd5596aa12d1c17fe3f78e4c2c6b7c
Hongson123 <86751961+Hongson123@users.noreply.github.com>
2021-07-01 07:29:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Chi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Chi cứ đi nhầm đường
+    Lạc vào tim Sơn lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng Chi cũng thích Sơn',
+    desc: 'Phải chăng chi cũng có tình cảm đặc biệt đó giống Sơn ',
+    btnYes: 'gật gật đúng rồi Sơn',
+    btnNo: 'Không, phang chết cha m giờ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Chi lại thích sơn <3',
+    btnReply: 'Gửi cho Sơn <3',
+    reply: 'Thích thì Thích mà không Thích thì Thích <33333333',
+    mess: 'Sơn biết mà 🥰. Thích Chi Nhất luôn. Chi xinh với cute vaizz 😘😘',
+    messDesc: 'Vậy thì giờ Ngày nào Chi cũng hát cho Sơn nghe nhá.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
